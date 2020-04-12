5c71ae61c2709f7ce3aeb7132cb8969f8ce21ce6
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-12 17:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, changeCompletely) {
 
-  
+
 }
 
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
 
-  
+
 }
